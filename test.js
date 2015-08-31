741335591033c17f5cf1e43c6c8f9ac3aa4aac66
Joakim Carlstein <joakim.carlstein@gmail.com>
2015-08-31 19:59:01 +0200

    Bump `ava` and `xo`

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var fn = require('./');
 
 test('.lintText()', function (t) {
 	var results = fn.lintText('\'use strict\';\nconsole.log("unicorn");\n').results;
-	t.assert(results[0].messages[0].ruleId === 'quotes');
+	t.is(results[0].messages[0].ruleId, 'quotes');
 	t.end();
 });
 
