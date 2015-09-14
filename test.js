c9e089d2deea1f360a9c618f5d7545ea51eee28c
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-14 16:56:14 +0700

    ES2015ify the tests

--- test.js
+++ test.js
@@ -1,21 +1,20 @@
-'use strict';
-var test = require('ava');
-var fn = require('./');
+import test from 'ava';
+import fn from './';
 
-test('.lintText()', function (t) {
-	var results = fn.lintText('\'use strict\';\nconsole.log("unicorn");\n').results;
+test('.lintText()', t => {
+	const results = fn.lintText('\'use strict\';\nconsole.log("unicorn");\n').results;
 	t.is(results[0].messages[0].ruleId, 'quotes');
 	t.end();
 });
 
-test('.lintText() - `esnext` option', function (t) {
-	var results = fn.lintText('function dec() {}\nconst x = {\n\t@dec()\n\ta: 1\n};\n', {esnext: true}).results;
+test('.lintText() - `esnext` option', t => {
+	const results = fn.lintText('function dec() {}\nconst x = {\n\t@dec()\n\ta: 1\n};\n', {esnext: true}).results;
 	t.is(results[0].messages[0].ruleId, 'no-unused-vars');
 	t.end();
 });
 
-test('.lintText() - JSX support', function (t) {
-	var results = fn.lintText('var app = <div className="appClass">Hello, React!</div>;\n', {esnext: false}).results;
+test('.lintText() - JSX support', t => {
+	const results = fn.lintText('var app = <div className="appClass">Hello, React!</div>;\n', {esnext: false}).results;
 	t.is(results[0].messages[0].ruleId, 'no-unused-vars');
 	t.end();
 });
