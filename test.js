360d37365440f4c6439d47729c9d332509a501bf
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-23 17:03:45 +0700

    eslint@1.7.3

--- test.js
+++ test.js
@@ -1,21 +1,25 @@
 import test from 'ava';
 import fn from './';
 
+function hasRule(results, ruleId) {
+	return results[0].messages.some(x => x.ruleId === ruleId);
+}
+
 test('.lintText()', t => {
 	const results = fn.lintText('\'use strict\';\nconsole.log("unicorn");\n').results;
-	t.is(results[0].messages[0].ruleId, 'quotes');
+	t.true(hasRule(results, 'quotes'));
 	t.end();
 });
 
 test('.lintText() - `esnext` option', t => {
 	const results = fn.lintText('function dec() {}\nconst x = {\n\t@dec()\n\ta: 1\n};\n', {esnext: true}).results;
-	t.is(results[0].messages[0].ruleId, 'no-unused-vars');
+	t.true(hasRule(results, 'no-unused-vars'));
 	t.end();
 });
 
 test('.lintText() - JSX support', t => {
 	const results = fn.lintText('var app = <div className="appClass">Hello, React!</div>;\n', {esnext: false}).results;
-	t.is(results[0].messages[0].ruleId, 'no-unused-vars');
+	t.true(hasRule(results, 'no-unused-vars'));
 	t.end();
 });
 
@@ -24,15 +28,13 @@ test('.lintText() - plugin support', t => {
 		plugins: ['react'],
 		rules: {'react/jsx-no-undef': 2}
 	}).results;
-	t.is(results[0].messages[0].ruleId, 'react/jsx-no-undef');
-	t.is(results[0].messages[0].message, '\'App\' is not defined.');
+	t.true(hasRule(results, 'react/jsx-no-undef'));
 	t.end();
 });
 
 test('.lintText() - prevent use of extended native objects', t => {
 	const results = fn.lintText('[].unicorn();\n').results;
-	t.is(results[0].messages[0].ruleId, 'no-use-extend-native/no-use-extend-native');
-	t.is(results[0].messages[0].message, 'Avoid using extended native objects');
+	t.true(hasRule(results, 'no-use-extend-native/no-use-extend-native'));
 	t.end();
 });
 
@@ -40,8 +42,7 @@ test('.lintText() - extends support', t => {
 	const results = fn.lintText('var React;\nReact.render(<App/>);\n', {
 		extends: 'xo-react'
 	}).results;
-	t.is(results[0].messages[0].ruleId, 'react/jsx-no-undef');
-	t.is(results[0].messages[0].message, '\'App\' is not defined.');
+	t.true(hasRule(results, 'react/jsx-no-undef'));
 	t.end();
 });
 
