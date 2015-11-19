3ec8aa1c9b1ab974acbf2ef946ffc42aa685e21b
Sindre Sorhus <sindresorhus@gmail.com>
2015-11-19 14:04:29 +0700

    fix travis

--- test/cli.js
+++ test/cli.js
@@ -5,11 +5,8 @@ import test from 'ava';
 import pify from 'pify';
 import tempWrite from 'temp-write';
 
-const execFile = pify(childProcess.execFile);
-const cli = path.join(__dirname, '..', 'cli.js');
-
 test('fix option', async t => {
 	const filepath = await tempWrite('var foo = 0; foo ++;', 'fix.js');
-	await execFile(cli, ['--fix', filepath]);
+	await pify(childProcess.execFile, Promise)('../cli.js', ['--fix', filepath]);
 	t.is(fs.readFileSync(filepath, 'utf8').trim(), 'var foo = 0; foo++;');
 });
