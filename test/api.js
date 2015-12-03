41a0cef229945a9028c3390081b2ac77e1c5e7bf
Brekk <brekk@brekkbockrath.com>
2015-12-03 13:25:13 -0800

    Amends previous.

--- test/api.js
+++ test/api.js
@@ -5,52 +5,45 @@ function hasRule(results, ruleId) {
 	return results[0].messages.some(x => x.ruleId === ruleId);
 }
 
-test.cb('.lintText()', t => {
+test('.lintText()', t => {
 	const results = fn.lintText('\'use strict\';\nconsole.log("unicorn");\n').results;
 	t.true(hasRule(results, 'quotes'));
-	t.end();
 });
 
-test.cb('.lintText() - `esnext` option', t => {
+test('.lintText() - `esnext` option', t => {
 	const results = fn.lintText('function dec() {}\nconst x = {\n\t@dec()\n\ta: 1\n};\n', {esnext: true}).results;
 	t.true(hasRule(results, 'no-unused-vars'));
-	t.end();
 });
 
-test.cb('.lintText() - JSX support', t => {
+test('.lintText() - JSX support', t => {
 	const results = fn.lintText('var app = <div className="appClass">Hello, React!</div>;\n', {esnext: false}).results;
 	t.true(hasRule(results, 'no-unused-vars'));
-	t.end();
 });
 
-test.cb('.lintText() - plugin support', t => {
+test('.lintText() - plugin support', t => {
 	const results = fn.lintText('var React;\nReact.render(<App/>);\n', {
 		plugins: ['react'],
 		rules: {'react/jsx-no-undef': 2}
 	}).results;
 	t.true(hasRule(results, 'react/jsx-no-undef'));
-	t.end();
 });
 
 // test('.lintText() - prevent use of extended native objects', t => {
 // 	const results = fn.lintText('[].unicorn();\n').results;
 // 	t.true(hasRule(results, 'no-use-extend-native/no-use-extend-native'));
-// 	t.end();
 // });
 
-test.cb('.lintText() - extends support', t => {
+test('.lintText() - extends support', t => {
 	const results = fn.lintText('var React;\nReact.render(<App/>);\n', {
 		extends: 'xo-react'
 	}).results;
 	t.true(hasRule(results, 'react/jsx-no-undef'));
-	t.end();
 });
 
-test.cb('.lintText() - extends support with `esnext` option', t => {
+test('.lintText() - extends support with `esnext` option', t => {
 	const results = fn.lintText('import path from \'path\';\nvar React;\nReact.render(<App/>);\n', {
 		esnext: true,
 		extends: 'xo-react'
 	}).results;
 	t.true(hasRule(results, 'react/jsx-no-undef'));
-	t.end();
 });
