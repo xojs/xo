57bbdc83a4385e4914724f9383d256a88d8126d6
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-13 23:56:05 +0100

    re-enable the `no-use-extend-native` plugin

--- index.js
+++ index.js
@@ -32,9 +32,8 @@ var DEFAULT_CONFIG = {
 };
 
 var DEFAULT_PLUGINS = [
-	'no-empty-blocks'
-	// https://github.com/dustinspecker/eslint-plugin-no-use-extend-native/issues/16
-	// 'no-use-extend-native'
+	'no-empty-blocks',
+	'no-use-extend-native'
 ];
 
 function handleOpts(opts) {
