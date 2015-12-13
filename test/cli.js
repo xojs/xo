852e2dcc760f5caa89ccca7be196017f3aa54199
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-14 00:01:57 +0100

    simplify CLI tests

--- test/cli.js
+++ test/cli.js
@@ -1,12 +1,13 @@
 import fs from 'fs';
 import path from 'path';
-import childProcess from 'child_process';
 import test from 'ava';
-import pify from 'pify';
 import tempWrite from 'temp-write';
+import execa from 'execa';
+
+global.Promise = Promise;
 
 test('fix option', async t => {
 	const filepath = await tempWrite('var foo = 0; foo ++;', 'fix.js');
-	await pify(childProcess.execFile, Promise)('../cli.js', ['--fix', filepath]);
+	await execa('../cli.js', ['--fix', filepath]);
 	t.is(fs.readFileSync(filepath, 'utf8').trim(), 'var foo = 0; foo++;');
 });
