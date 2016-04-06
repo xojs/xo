d55d1cc73d06d8c2d3349f7e3626ae1434e4ddd7
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-07 02:37:50 +0700

    add `eslint-plugin-ava` ESLint plugin

--- config/plugins.js
+++ config/plugins.js
@@ -3,8 +3,10 @@ module.exports = {
 	plugins: [
 		'no-empty-blocks',
 		'no-use-extend-native',
-		'import-order'
+		'import-order',
+		'ava'
 	],
+	extends: 'plugin:ava/recommended',
 	rules: {
 		'no-empty-blocks/no-empty-blocks': [2, 'allowCatch'],
 		'no-use-extend-native/no-use-extend-native': 2,
