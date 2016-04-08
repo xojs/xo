8da6e5986e172e4b35b6e5f6fb5d057022c9fa4a
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-09 01:32:49 +0700

    add eslint-plugin-promise - fixes #98
    https://github.com/xjamundx/eslint-plugin-promise

--- config/plugins.js
+++ config/plugins.js
@@ -4,12 +4,14 @@ module.exports = {
 		'no-empty-blocks',
 		'no-use-extend-native',
 		'import-order',
-		'ava'
+		'ava',
+		'promise'
 	],
 	extends: 'plugin:ava/recommended',
 	rules: {
 		'no-empty-blocks/no-empty-blocks': [2, 'allowCatch'],
 		'no-use-extend-native/no-use-extend-native': 2,
-		'import-order/import-order': 2
+		'import-order/import-order': 2,
+		'promise/param-names': 2
 	}
 };
