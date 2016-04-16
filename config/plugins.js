0a43304e5f5e0101f72e11cc89a7f32f5871f7be
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-17 00:51:40 +0700

    remove the `no-empty-blocks` plugin
    no longer needed: https://github.com/sindresorhus/eslint-config-xo/commit/83991acef047dd08eb1eaa1e32d8775c560d0e8a

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,6 @@
 'use strict';
 module.exports = {
 	plugins: [
-		'no-empty-blocks',
 		'no-use-extend-native',
 		'import-order',
 		'ava',
@@ -13,7 +12,6 @@ module.exports = {
 		'plugin:xo/recommended'
 	],
 	rules: {
-		'no-empty-blocks/no-empty-blocks': [2, 'allowCatch'],
 		'no-use-extend-native/no-use-extend-native': 2,
 		'import-order/import-order': 2,
 		'promise/param-names': 2
