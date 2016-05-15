8fa7e6e87c1ecc02cb346ab0cde23b27adb6e8a0
Sindre Sorhus <sindresorhus@gmail.com>
2016-05-15 19:29:27 +0700

    fix some outdated config

--- config/overrides.js
+++ config/overrides.js
@@ -5,9 +5,13 @@ module.exports = {
 	parser: 'babel-eslint',
 	plugins: ['babel'],
 	rules: {
-		'generator-star-spacing': 0,
-		'arrow-parens': 0,
+		'new-cap': 0,
+		'array-bracket-spacing': 0,
 		'object-curly-spacing': 0,
-		'babel/object-curly-spacing': [2, 'never']
+		'arrow-parens': 0,
+		'babel/new-cap': [2, {newIsCap: true, capIsNew: true}],
+		'babel/array-bracket-spacing': [2, 'never'],
+		'babel/object-curly-spacing': [2, 'never'],
+		'babel/arrow-parens': [2, 'as-needed']
 	}
 };
