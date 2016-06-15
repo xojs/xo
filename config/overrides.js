c79c49c7bae637642c3e767bf6eb8f2f1b583699
Sindre Sorhus <sindresorhus@gmail.com>
2016-06-15 16:19:06 +0200

    forgot to disable the default `generator-star-spacing` rule in favor of the Babel one
    Especially important because of https://github.com/babel/babel-eslint/issues/316

--- config/overrides.js
+++ config/overrides.js
@@ -5,10 +5,12 @@ module.exports = {
 	parser: 'babel-eslint',
 	plugins: ['babel'],
 	rules: {
+		'generator-star-spacing': 0,
 		'new-cap': 0,
 		'array-bracket-spacing': 0,
 		'object-curly-spacing': 0,
 		'arrow-parens': 0,
+		'babel/generator-star-spacing': [2, 'both'],
 		'babel/new-cap': [2, {newIsCap: true, capIsNew: true}],
 		'babel/array-bracket-spacing': [2, 'never'],
 		'babel/object-curly-spacing': [2, 'never'],
