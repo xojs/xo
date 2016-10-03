e8b0a30eb9f27f73e95e118ef1e1e8a3dacb12f9
Sindre Sorhus <sindresorhus@gmail.com>
2016-10-03 12:37:50 +0700

    upgrade to eslint-plugin-import@2

--- config/plugins.js
+++ config/plugins.js
@@ -9,6 +9,7 @@ module.exports = {
 			experimentalObjectRestSpread: true
 		}
 	},
+	// -- end repeat
 	plugins: [
 		'no-use-extend-native',
 		'ava',
@@ -21,25 +22,31 @@ module.exports = {
 		'plugin:unicorn/recommended'
 	],
 	settings: {
-		'import/extensions': ['.js'] // TODO: remove this when eslint-plugin-import@2 is out
+		'import/core-modules': [
+			'electron',
+			'atom'
+		]
 	},
 	rules: {
 		'no-use-extend-native/no-use-extend-native': 2,
 		'promise/param-names': 2,
-		// disabled because of https://github.com/benmosher/eslint-plugin-import/issues/268
-		// 'import/default': 2,
+		'import/default': 2,
 		'import/export': 2,
 		'import/extensions': [2, {
 			js: 'never',
 			json: 'never',
 			jsx: 'never'
 		}],
-		'import/imports-first': 2,
-		// disabled because of https://github.com/benmosher/eslint-plugin-import/issues/268
-		// 'import/named': 2,
-		'import/namespace': 2,
+		'import/first': 2,
+		'import/named': 2,
+		'import/namespace': [2, {allowComputed: true}],
+		'import/no-absolute-path': 2,
+		'import/no-dynamic-require': 2,
+		'import/no-webpack-loader-syntax': 2,
 		'import/newline-after-import': 2,
 		'import/no-amd': 2,
+		// enable this sometime in the future when Node.js has ES2015 module support
+		// 'import/unambiguous': 2,
 		// enable this sometime in the future when Node.js has ES2015 module support
 		// 'import/no-commonjs': 2,
 		// looks useful, but too unstable at the moment
@@ -48,8 +55,9 @@ module.exports = {
 		'import/no-mutable-exports': 2,
 		'import/no-named-as-default-member': 2,
 		'import/no-named-as-default': 2,
-		// disabled because of https://github.com/benmosher/eslint-plugin-import/issues/275
-		// 'import/no-unresolved': [2, {commonjs: true}],
-		'import/order': 2
+		'import/no-unresolved': [2, {commonjs: true}],
+		'import/order': 2,
+		'import/prefer-default-export': 2,
+		'import/no-unassigned-import': 2
 	}
 };
