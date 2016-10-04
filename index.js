c5d7ceb67717cc18ee70794243697d7efaff06df
André Breda <addobandre99@gmail.com>
2016-10-04 21:52:46 +0100

    Add anyExtension option
    Turn it on and you can lint files with any extension.
    That is, the filter ignoring all files that do not end
    in .js or .jsx is disabled.

--- index.js
+++ index.js
@@ -33,10 +33,12 @@ exports.lintFiles = (patterns, opts) => {
 
 	return globby(patterns, {ignore: opts.ignores}).then(paths => {
 		// when users are silly and don't specify an extension in the glob pattern
-		paths = paths.filter(x => {
-			const ext = path.extname(x);
-			return ext === '.js' || ext === '.jsx';
-		});
+		if (!opts.anyExtension) {
+			paths = paths.filter(x => {
+				const ext = path.extname(x);
+				return ext === '.js' || ext === '.jsx';
+			});
+		}
 
 		if (!(opts.overrides && opts.overrides.length > 0)) {
 			return runEslint(paths, opts);
