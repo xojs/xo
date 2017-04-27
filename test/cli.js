30d452408f288d35963cbde384ffbf4a7fb26f68
Mario Nebl <hello@herebecode.com>
2017-04-27 10:46:15 -0700

    test: sanitize entry file resolving

--- test/cli.js
+++ test/cli.js
@@ -6,21 +6,26 @@ import execa from 'execa';
 
 process.chdir(__dirname);
 
+const cli = (...args) => {
+	const cliPath = path.join(__dirname, '../cli.js');
+	return execa(...[cliPath, ...args]);
+};
+
 test('fix option', async t => {
 	const filepath = await tempWrite('console.log()\n', 'x.js');
-	await execa('../cli.js', ['--no-local', '--fix', filepath]);
+	await cli(['--no-local', '--fix', filepath]);
 	t.is(fs.readFileSync(filepath, 'utf8').trim(), 'console.log();');
 });
 
 test('fix option with stdin', async t => {
-	const {stdout} = await execa('../cli.js', ['--no-local', '--fix', '--stdin'], {
+	const {stdout} = await cli(['--no-local', '--fix', '--stdin'], {
 		input: 'console.log()\n'
 	});
 	t.is(stdout.trim(), 'console.log();');
 });
 
 test('stdin-filename option with stdin', async t => {
-	const {stdout} = await execa('../cli.js', ['--no-local', '--stdin', '--stdin-filename=unicorn-file'], {
+	const {stdout} = await cli(['--no-local', '--stdin', '--stdin-filename=unicorn-file'], {
 		input: 'console.log()\n',
 		reject: false
 	});
@@ -31,7 +36,7 @@ test('reporter option', async t => {
 	const filepath = await tempWrite('console.log()\n', 'x.js');
 
 	try {
-		await execa('../cli.js', ['--no-local', '--reporter=compact', filepath]);
+		await cli(['--no-local', '--reporter=compact', filepath]);
 	} catch (err) {
 		t.true(err.stdout.indexOf('Error - ') !== -1);
 	}
@@ -39,20 +44,20 @@ test('reporter option', async t => {
 
 test('overrides fixture', async t => {
 	const cwd = path.join(__dirname, 'fixtures/overrides');
-	await t.notThrows(execa('../../../cli.js', ['--no-local'], {cwd}));
+	await t.notThrows(cli(['--no-local'], {cwd}));
 });
 
 // https://github.com/sindresorhus/xo/issues/65
 test.failing('ignores fixture', async t => {
 	const cwd = path.join(__dirname, 'fixtures/ignores');
-	await t.throws(execa('../../../cli.js', ['--no-local'], {cwd}));
+	await t.throws(cli(['--no-local'], {cwd}));
 });
 
 test('ignore files in .gitignore', async t => {
 	const cwd = path.join(__dirname, 'fixtures/gitignore');
 
 	try {
-		await execa('../../../cli.js', ['--no-local'], {cwd});
+		await cli(['--no-local'], {cwd});
 	} catch (err) {
 		t.is(err.stdout.indexOf('foo.js'), -1);
 		t.true(err.stdout.indexOf('bar.js') !== -1);
@@ -61,11 +66,11 @@ test('ignore files in .gitignore', async t => {
 
 test('supports being extended with a shareable config', async t => {
 	const cwd = path.join(__dirname, 'fixtures/project');
-	await t.notThrows(execa('../../../cli.js', ['--no-local'], {cwd}));
+	await t.notThrows(cli(['--no-local'], {cwd}));
 });
 
 test('quiet option', async t => {
 	const filepath = await tempWrite('// TODO: quiet\nconsole.log()\n', 'x.js');
-	const err = await t.throws(execa('../cli.js', ['--no-local', '--quiet', '--reporter=compact', filepath]));
+	const err = await t.throws(cli(['--no-local', '--quiet', '--reporter=compact', filepath]));
 	t.is(err.stdout.indexOf('warning'), -1);
 });
