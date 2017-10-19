1daeac5f688b9ac90bc4caced66febfc33cd7380
Sindre Sorhus <sindresorhus@gmail.com>
2017-10-19 23:21:39 +0700

    Don't dogfood XO here
    Causes too many weird problems.

--- test/cli.js
+++ test/cli.js
@@ -6,11 +6,6 @@ const cwd = path.dirname(__dirname);
 const packageJson = require(path.join(cwd, 'package.json'));
 const cli = (args, opts) => execa(path.join(cwd, 'cli.js'), args, opts);
 
-test('prefer local install of XO', async t => {
-	const {stdout} = await cli(['--version'], {cwd});
-	t.is(`sindresorhus/xo#v${stdout}`, packageJson.devDependencies.xo);
-});
-
 test('runs no-local install of XO', async t => {
 	const {stdout} = await cli(['--no-local', '--version'], {cwd});
 	t.is(stdout, packageJson.version);
