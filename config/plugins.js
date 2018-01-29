61a85cfe1dce4607db667085d53afc209d3eb5e0
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-01-29 12:34:51 -0500

    Revert JSDoc plugin (#291)
    This reverts commit c1f1be647fda10b8c22a9473a3e14e99089ba48a.

--- config/plugins.js
+++ config/plugins.js
@@ -16,8 +16,7 @@ module.exports = {
 		'unicorn',
 		'promise',
 		'import',
-		'node',
-		'jsdoc'
+		'node'
 	],
 	extends: [
 		'plugin:ava/recommended',
@@ -81,9 +80,6 @@ module.exports = {
 		// 'node/shebang': 'error',
 		'node/no-deprecated-api': 'error',
 		'node/exports-style': ['error', 'module.exports'],
-		'jsdoc/newline-after-description': 'warn',
-		'jsdoc/require-description-complete-sentence': 'warn',
-		'jsdoc/require-hyphen-before-param-description': 'warn',
 		// Disabled by default (overrides `plugin:unicorn/recommended`), will be enabled if supported by the Node.js version
 		'unicorn/prefer-spread': 'off',
 		'unicorn/no-new-buffer': 'off'
