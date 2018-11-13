61f9582c0d17db0d68acff35fb060ceb01856e0b
Sindre Sorhus <sindresorhus@gmail.com>
2018-11-13 13:52:57 +0700

    Support parsing ES2019

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,7 @@
 module.exports = {
 	// Repeated here from eslint-config-xo in case some plugins set something different
 	parserOptions: {
-		ecmaVersion: 2018,
+		ecmaVersion: 2019,
 		sourceType: 'module',
 		ecmaFeatures: {
 			jsx: true
