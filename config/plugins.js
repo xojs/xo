b0ebabd251c3311fcdb127a0a7f1e31f0fbac4a4
Sindre Sorhus <sindresorhus@gmail.com>
2020-10-17 12:42:10 +0200

    Update dependencies

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,7 @@
 module.exports = {
 	// Repeated here from eslint-config-xo in case some plugins set something different
 	parserOptions: {
-		ecmaVersion: 2020,
+		ecmaVersion: 2021,
 		sourceType: 'module',
 		ecmaFeatures: {
 			jsx: true
