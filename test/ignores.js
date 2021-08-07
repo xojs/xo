bc672d6a68fb864d1be25df41a8826fd26dd1d39
fisker Cheung <lionkay@gmail.com>
2021-08-07 10:50:41 +0800

    Improve tests

--- test/ignores.js
+++ test/ignores.js
@@ -1,7 +1,7 @@
-
 import path from 'node:path';
 import test from 'ava';
 import createEsmUtils from 'esm-utils';
+import {globby} from 'globby';
 import xo from '../index.js';
 
 const {__dirname} = createEsmUtils(import.meta);
@@ -9,17 +9,32 @@ const {__dirname} = createEsmUtils(import.meta);
 test('Should pickup "ignores" config', async t => {
 	const cwd = path.join(__dirname, 'fixtures/nested-ignores');
 
-	// Should not match `a.js`, `child/a.js`, and `child/b.js`
+	t.deepEqual(
+		await globby(['**/*.js'], {cwd}),
+		['a.js', 'b.js', 'child/a.js', 'child/b.js'],
+		'Should has 4 js files.',
+	);
+
+	// Should not match
+	// `a.js` (ignored by config in current directory)
+	// `child/a.js` (ignored by config in current directory)
+	// `child/b.js` (ignored by config in child directory)
 	const result = await xo.lintFiles('.', {cwd});
 	const files = result.results.map(({filePath}) => filePath);
-	t.deepEqual(files, [path.join(cwd, 'b.js')]);
+	t.deepEqual(files, [path.join(cwd, 'b.js')], 'Should only report on `b.js`.');
 });
 
 test('Should ignore "ignores" config in parent', async t => {
 	const cwd = path.join(__dirname, 'fixtures/nested-ignores/child');
 
-	// Should matches `a.js` even it's ignored in parent
+	t.deepEqual(
+		await globby(['**/*.js'], {cwd}),
+		['a.js', 'b.js'],
+		'Should has 2 js files.',
+	);
+
+	// Should only match `a.js` even it's ignored in parent
 	const result = await xo.lintFiles('.', {cwd});
 	const files = result.results.map(({filePath}) => filePath);
-	t.deepEqual(files, [path.join(cwd, 'a.js')]);
+	t.deepEqual(files, [path.join(cwd, 'a.js')], 'Should only report on `a.js`.');
 });
