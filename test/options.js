61c4aeee6d8cec0e1a49fe8b810fcb77cb713ad0
fisker Cheung <lionkay@gmail.com>
2021-08-08 19:12:08 +0800

    Style

--- test/options.js
+++ test/options.js
@@ -1,26 +1,24 @@
-import process from 'node:process';
-import path from 'node:path';
 import test from 'ava';
 import xo from '../index.js';
 
-test('prettier', async (t) => {
+test('prettier', async t => {
 	const config = await xo.getConfig({prettier: true, filePath: 'example.js'});
 
 	t.is(
 		config.rules['arrow-body-style'][0],
 		'off',
-		'Should extends `eslint-plugin-prettier`, not `eslint-config-prettier`.'
+		'Should extends `eslint-plugin-prettier`, not `eslint-config-prettier`.',
 	);
 
 	t.is(
 		config.rules['unicorn/empty-brace-spaces'][0],
 		'off',
-		'`unicorn/empty-brace-spaces` should be turned off by prettier.'
+		'`unicorn/empty-brace-spaces` should be turned off by prettier.',
 	);
 
 	t.is(
 		config.rules['@typescript-eslint/brace-style'][0],
 		'off',
-		'`@typescript-eslint/brace-style` should be turned off even we are not using TypeScript.'
+		'`@typescript-eslint/brace-style` should be turned off even we are not using TypeScript.',
 	);
-})
+});
